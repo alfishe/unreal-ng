b108f7129fb05c033a2f4cbe59cc07606454d8c0
Ilia Sharin <isharin@gmail.com>
2020-03-29 22:18:23 -0400

    Fixed for non MSVC / Win compilers

--- core/src/stdafx.cpp
+++ core/src/stdafx.cpp
@@ -3,4 +3,15 @@
 // Fake export for stdafx.o to get rid of MSVC linker error.
 // LNK4221: This object file does not define any previously undefined public symbols,
 // so it will not be used by any link operation that consumes this library
+#if defined _WIN32 || defined __CYGWIN__ || defined __MINGW32__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 __declspec( dllexport ) void getRidOfLNK4221(){}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
