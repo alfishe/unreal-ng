c2bfada0e87ea3a01793b26a81b9c2e8892605e7
Ilia Sharin <isharin@gmail.com>
2020-06-28 21:12:06 -0400

    Logging adjustments when running unit tests / benchmarks

--- core/src/common/logger.h
+++ core/src/common/logger.h
@@ -6,11 +6,25 @@
 
 using namespace std;
 
+#ifndef _CODE_UNDER_TEST
+
+// Write all Debug / Info logs if not under unit testing / benchmarking
 #define LOGDEBUG(format, ...) Logger::Debug(format, ##__VA_ARGS__)
 #define LOGINFO(format, ...) Logger::Info(format, ##__VA_ARGS__)
 #define LOGWARNING(format, ...) Logger::Warning(format, ##__VA_ARGS__)
 #define LOGERROR(format, ...) Logger::Error(format, ##__VA_ARGS__)
 #define LOGEMPTY(...) Logger::EmptyLine(##__VA_ARGS__)
+
+#else
+
+// No Debug / Info for unit tests and benchmarks
+#define LOGDEBUG(format, ...)
+#define LOGINFO(format, ...)
+#define LOGWARNING(format, ...) Logger::Warning(format, ##__VA_ARGS__)
+#define LOGERROR(format, ...) Logger::Error(format, ##__VA_ARGS__)
+#define LOGEMPTY(...) Logger::EmptyLine(##__VA_ARGS__)
+
+#endif
 
 
 class Logger
