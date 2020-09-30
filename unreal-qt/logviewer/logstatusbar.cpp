927dbb34a054371bd911ad0b8a781ea72ae0ee39
Ilia Sharin <isharin@gmail.com>
2020-09-29 20:09:05 -0400

    Missing files added

---
+++ unreal-qt/logviewer/logstatusbar.cpp
@@ -0,0 +1,11 @@
+#include "logstatusbar.h"
+
+LogStatusBar::LogStatusBar()
+{
+
+}
+
+LogStatusBar::~LogStatusBar()
+{
+
+}
---
+++ unreal-qt/logviewer/logstatusbar.h
@@ -0,0 +1,14 @@
+#ifndef LOGSTATUSBAR_H
+#define LOGSTATUSBAR_H
+
+#include <QWidget>
+
+class LogStatusBar
+{
+    Q_OBJECT
+public:
+    LogStatusBar();
+    virtual ~LogStatusBar();
+};
+
+#endif // LOGSTATUSBAR_H
