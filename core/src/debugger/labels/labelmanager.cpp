ecdc5452e624ffee1d6a0a4ac7419395e597a3d9
Ilia Sharin <isharin@gmail.com>
2020-10-23 16:42:33 -0400

    Added label manager class

---
+++ core/src/debugger/labels/labelmanager.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "labelmanager.h"
+
+#include "common/modulelogger.h"
---
+++ core/src/debugger/labels/labelmanager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "stdafx.h"
+
+class ModuleLogger;
+
+class LabelManager
+{
+
+};
