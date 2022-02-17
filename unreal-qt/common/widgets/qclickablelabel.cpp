87d5c08c68d748cceb63569dc535c16e8f80c49c
Ilia Sharin <isharin@gmail.com>
2022-02-17 14:31:02 -0500

    Optimized source code for QClickableLabel class

--- unreal-qt/common/widgets/qclickablelabel.cpp
+++ unreal-qt/common/widgets/qclickablelabel.cpp
@@ -1,13 +1,5 @@
 #include "qclickablelabel.h"
 
-QClickableLabel::QClickableLabel(QWidget* parent, Qt::WindowFlags f) : QLabel(parent)
-{
-}
-
-QClickableLabel::~QClickableLabel()
-{
-}
-
 void QClickableLabel::mousePressEvent(QMouseEvent* event)
 {
     emit clicked();
--- unreal-qt/common/widgets/qclickablelabel.h
+++ unreal-qt/common/widgets/qclickablelabel.h
@@ -12,19 +12,23 @@ class QClickableLabel : public QLabel
 {
     Q_OBJECT
 
+    /// region <Constructors / destructors>
 public:
-    explicit QClickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
-    ~QClickableLabel();
+    explicit QClickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags()) : QLabel(parent) {};
+    virtual ~QClickableLabel() = default;
+    /// endregion </Constructors / destructors>
 
 signals:
     void clicked();
     void released();
     void doubleClicked();
 
+    /// region <Event handlers>
 protected:
     void mousePressEvent(QMouseEvent* event);
     void mouseReleaseEvent(QMouseEvent* event);
     void mouseDoubleClickEvent(QMouseEvent* event);
+    /// endregion </Event handlers>
 };
 
 
