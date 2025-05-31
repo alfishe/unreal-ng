1eeb9dbaaf80c511961ba4341046bc11e65956db
Ilia Sharin <isharin@gmail.com>
2025-05-31 14:33:52 -0400

    Initial installer packages build process

--- core/automation/webapi/src/automation-webapi.cpp
+++ core/automation/webapi/src/automation-webapi.cpp
@@ -1,11 +1,13 @@
 #include "automation-webapi.h"
 
-#include <thread>
 #include <drogon/HttpController.h>
 #include <drogon/WebSocketController.h>
-#include "hello_world_api.h"        // Triggers auto-registration for API handlers
-#include "emulator_api.h"           // Triggers auto-registration for API handlers
-#include "emulator_websocket.h"     // Triggers auto-registration for WebSocket handlers
+
+#include <thread>
+
+#include "emulator_api.h"        // Triggers auto-registration for API handlers
+#include "emulator_websocket.h"  // Triggers auto-registration for WebSocket handlers
+#include "hello_world_api.h"     // Triggers auto-registration for API handlers
 
 /// region <Methods>
 void AutomationWebAPI::start()
@@ -48,36 +50,36 @@ void AutomationWebAPI::threadFunc(AutomationWebAPI* webApi)
     pthread_setname_np(threadName);
 #endif
 #ifdef __linux__
-    #include <pthread.h>
-	pthread_setname_np(pthread_self(), threadName);
+#include <pthread.h>
+    pthread_setname_np(pthread_self(), threadName);
 #endif
 #if defined _WIN32 && defined MSVC
     static auto setThreadDescription = reinterpret_cast<HRESULT(WINAPI*)(HANDLE, PCWSTR)>(
         GetProcAddress(GetModuleHandle("kernelbase.dll"), "SetThreadDescription"));
     if (setThreadDescription != nullptr)
     {
-	    wchar_t wname[24];
-	    size_t retval;
+        wchar_t wname[24];
+        size_t retval;
         mbstate_t conversion;
-        mbstowcs_s(&retval, wname, threadName, sizeof (threadName) / sizeof (threadName[0]), &conversion);
+        mbstowcs_s(&retval, wname, threadName, sizeof(threadName) / sizeof(threadName[0]), &conversion);
         setThreadDescription(GetCurrentThread(), wname);
     }
 #endif
 
 #if defined _WIN32 && defined __GNUC__
     static auto setThreadDescription = reinterpret_cast<HRESULT(WINAPI*)(HANDLE, PCWSTR)>(
-            GetProcAddress(GetModuleHandle("kernelbase.dll"), "SetThreadDescription"));
+        GetProcAddress(GetModuleHandle("kernelbase.dll"), "SetThreadDescription"));
     if (setThreadDescription != nullptr)
     {
         wchar_t wname[24];
         size_t retval;
         mbstate_t conversion;
-        mbsrtowcs_s(&retval, wname, (size_t)(sizeof (wname) / sizeof (wname[0])), &threadName, (size_t)(sizeof (threadName) / sizeof (threadName[0])), &conversion);
+        mbsrtowcs_s(&retval, wname, (size_t)(sizeof(wname) / sizeof(wname[0])), &threadName,
+                    (size_t)(sizeof(threadName) / sizeof(threadName[0])), &conversion);
         setThreadDescription(GetCurrentThread(), wname);
     }
 #endif
     /// endregion </Make thread named for easy reading in debuggers>
-
 
     LOG_INFO << "Starting server on port 8090.";
     LOG_INFO << "HTTP API example: http://localhost:8090/api/v1/HelloWorld";
@@ -86,9 +88,33 @@ void AutomationWebAPI::threadFunc(AutomationWebAPI* webApi)
 
     drogon::HttpAppFramework& app = drogon::app();
 
-    app.setLogPath("./")
+    // Create a writable log directory in the user's home folder
+    std::string logPath;
+#ifdef __APPLE__
+    // On macOS, use ~/Library/Logs/UnrealNG/
+    const char* homeDir = getenv("HOME");
+    if (homeDir)
+    {
+        logPath = std::string(homeDir) + "/Library/Logs/UnrealNG";
+        // Create the directory if it doesn't exist
+        std::filesystem::create_directories(logPath);
+    }
+    else
+    {
+        // Fallback to temporary directory if HOME is not available
+        logPath = "/tmp/UnrealNG";
+        std::filesystem::create_directories(logPath);
+    }
+#else
+    // On other platforms, use the current directory
+    logPath = "./";
+#endif
+
+    LOG_INFO << "Using log path: " << logPath;
+
+    app.setLogPath(logPath)
         .setLogLevel(trantor::Logger::kNumberOfLogLevels)
-        .disableSigtermHandling() // SIGTERM is handled by the main application (unreal-qt or testclient)
+        .disableSigtermHandling()  // SIGTERM is handled by the main application (unreal-qt or testclient)
         .addListener("0.0.0.0", 8090)
         .setThreadNum(2)
         .run();
