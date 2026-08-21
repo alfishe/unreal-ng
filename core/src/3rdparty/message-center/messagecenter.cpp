b74cb4d996b4634f11ebe081206e9ac0ec11049a
Ilia Sharin <isharin@gmail.com>
2026-08-20 22:53:12 -0400

    Fix Linux pthread_setname_np: add pthread_self() argument

--- core/src/3rdparty/message-center/messagecenter.cpp
+++ core/src/3rdparty/message-center/messagecenter.cpp
@@ -128,7 +128,7 @@ void MessageCenter::ThreadWorker()
 #endif
 #ifdef __linux__
     #include <pthread.h>
-	pthread_setname_np(threadName);
+	pthread_setname_np(pthread_self(), threadName);
 #endif
 #ifdef _WIN32
     using SetThreadDescriptionFunc = HRESULT(WINAPI*)(HANDLE, PCWSTR);
