addd955f591cc3359aa8255f7d3b6655f1d6f031
Ilia Sharin <isharin@gmail.com>
2026-09-08 19:24:42 -0400

    Force DeviceScreen repaint on resize

--- unreal-qt/src/widgets/devicescreen.cpp
+++ unreal-qt/src/widgets/devicescreen.cpp
@@ -318,6 +318,8 @@ void DeviceScreen::resizeEvent(QResizeEvent* event)
     resize(newWidth, newHeight);
 
     QWidget::resizeEvent(event);
+
+    update();
 }
 
 void DeviceScreen::prepareForShutdown()
